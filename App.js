28b4fa8c8addf80eaf60bde87b58e55d42588bf3
Venomity <veeti.hietasalo@hotmail.fi>
2023-09-06 17:35:21 +0300

    palautus

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Button, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [ostos, setOstos] = useState('');
+  const [ostoslista, setOstoslista] = useState([]);
+
+  const lisaaOstos = () => {
+    if (ostos.length > 0) {
+      setOstoslista([...ostoslista, { key: Math.random().toString(), value: ostos }]);
+      setOstos('');
+    }
+  };
+
+  const tyhjennaLista = () => {
+    setOstoslista([]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Syötä ostos"
+          style={styles.inputField}
+          onChangeText={setOstos}
+          value={ostos}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="Add" onPress={lisaaOstos} />
+          <Button title="Clear" onPress={tyhjennaLista} color="red" />
+        </View>
+      </View>
+      <FlatList
+        data={ostoslista}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputField: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '20%'
+  },
+  listItem: {
+    padding: 5,
+    marginVertical: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
+
